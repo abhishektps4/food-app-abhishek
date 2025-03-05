faa07354021a622b9e196ba803439fbe1f0ce22e
abhishektps4 <abhishek1720044@gmail.com>
2025-03-05 09:34:30 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://abhishek1720044:abhishek123@cluster0.oayt8.mongodb.net').then(()=>console.log("DB Connected"));
+    await mongoose.connect('').then(()=>console.log("DB Connected"));
 }
